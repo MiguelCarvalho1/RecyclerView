db7af84e454850e67e14a5d81ebe10694c45c15d
migue <miguelangelosilvacarvalho96@gmail.com>
2021-03-04 22:34:11 +0000

    erros no MainActivity2.kt falta layou-land o insert

--- app/src/main/java/com/example/recyclerview/MainActivity.kt
+++ app/src/main/java/com/example/recyclerview/MainActivity.kt
@@ -1,32 +1,75 @@
 package com.example.recyclerview
 
-import LineAdapter.LineAdapter
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
-import androidx.recyclerview.widget.LinearLayoutManager
-import dataclasses.Place
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
-const val PARAM1 = "city"
+const val PARAM1_NAME = "nome"
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var myList: ArrayList<Place>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Toast.makeText(this, R.string.welcome, Toast.LENGTH_SHORT).show()
+    }
 
-        myList = ArrayList<Place>()
+    override fun onPause() {
+        super.onPause()
+    }
 
-        for(i in 0 until 500){
-            myList.add(Place("Country $i", i*500, "Capital $i"))
+    fun button1(view: View) {
+
+        var edit1 = findViewById<EditText>(R.id.editTextTextPersonName)
+        Toast.makeText(this, edit1.text, Toast.LENGTH_SHORT).show()
+        findViewById<TextView>(R.id.txt1).setText(edit1.text)
+
+    }
+
+    fun button2(view: View) {
+        var edit1 = findViewById<EditText>(R.id.editTextTextPersonName)
+
+        val intent = Intent(this, MainActivity2::class.java).apply {
+            putExtra(PARAM1_NAME, edit1.text.toString())
         }
 
-        recycler_view.adapter = LineAdapter(myList)
-        recycler_view.layoutManager= LinearLayoutManager(this)
+        startActivity(intent)
     }
 
-    fun insert (view: View){
-        myList.add(0, Place("Country XXX", 999, "Capital XXX"))
-        recycler_view.adapter?.notifyDataSetChanged()
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return true
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle item selection
+        return when (item.itemId) {
+            R.id.create_new -> {
+                Toast.makeText(this, "@strings/app_name", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.opcao2 -> {
+                Toast.makeText(this, "opcao2", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.opcao3 -> {
+                Toast.makeText(this, "opcao3", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.opcao4 -> {
+                Toast.makeText(this, "opcao4", Toast.LENGTH_SHORT).show()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+
 }
